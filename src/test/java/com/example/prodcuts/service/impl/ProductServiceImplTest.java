b224dc6f867603d8fec8d7b49569427f856c9f41
Artur Muliukov <arthur.muliukov@gmail.com>
2023-07-12 14:01:49 +0200

    Changed yml file

---
+++ src/test/java/com/example/prodcuts/service/impl/ProductServiceImplTest.java
@@ -0,0 +1,15 @@
+package com.example.prodcuts.service.impl;
+
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class ProductServiceImplTest {
+    @Test
+    void name() {
+        Integer one = 1;
+        Integer expectedInt = 1;
+        Assertions.assertEquals(expectedInt,one);
+    }
+}
